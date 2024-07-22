e118d83a6ef6f6bea089b6adee86890264e7fef2
Programmer Pemula <98646763+Sreetx@users.noreply.github.com>
2024-07-22 07:14:30 +0700

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -18,23 +18,24 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 try:
     try:
-        import sys, os, time, optparse, webbrowser
-        from color.warna import orange
-        from color.warna import putih
-        from color.warna import merah
-        from color.warna import hijau
-        from color.warna import biru
-        from color.warna import borange
-        from color.warna import bputih
-        from color.warna import bhijau
-        from color.warna import bbiru
-        from  color.warna import bmerah
-        from color.warna import kelabu
-        from color.warna import borangekelip
-        from color.warna import banmerah
-        from color.warna import banhijau
-        from color.warna import banorange
-        from color.warna import reset
+        import sys, os, time, optparse, webbrowser, random, zlib
+        from Extras.banner import banner
+        from Extras.color.warna import orange
+        from Extras.color.warna import putih
+        from Extras.color.warna import merah
+        from Extras.color.warna import hijau
+        from Extras.color.warna import biru
+        from Extras.color.warna import borange
+        from Extras.color.warna import bputih
+        from Extras.color.warna import bhijau
+        from Extras.color.warna import bbiru
+        from Extras.color.warna import bmerah
+        from Extras.color.warna import kelabu
+        from Extras.color.warna import borangekelip
+        from Extras.color.warna import banmerah
+        from Extras.color.warna import banhijau
+        from Extras.color.warna import banorange
+        from Extras.color.warna import reset
     except ImportError:
         print(' [!] Harap install ulang script ini dari repository github kami!');sys.exit()
     os.system('mkdir hasil')
@@ -46,38 +47,29 @@ try:
         from tqdm import tqdm
         import datetime
         import socket
+
     except ImportError:
         print(putih+' ['+banmerah+'!'+reset+putih+']'+borangekelip+' Module tidak lengkap'+reset)
         print(putih+' ['+banmerah+'!'+reset+putih+']'+reset+orange+' Module akan segera diinstall'+reset);time.sleep(1)
         if sys.platform in ['win32', 'win64']:
             os.system('pip install rarfile')
             os.system('pip install tqdm')
+            os.system("pip install py7zr")
             sys.exit()
         if sys.platform in ['linux', 'linux2']:
             os.system('pip install rarfile')
             os.system('pip install tqdm')
+            os.system('pip install py7zr')
             print(borange+' [*] Jika modul terpasang dengan baik tekan CTR+C, mulai Ulang script'+reset)
             enter = print(borange+" [*] Jika ada masalah dengan pip tekan enter untuk menginstall nya (for Debian and derivative users)"+reset)
             os.system('apt install python3-rarfile')
             os.system('apt install python3-tqdm')
+            os.system('apt install python3-py7zr')
             sys.exit()
         else: pass
 except KeyboardInterrupt: print(bputih+' ['+banmerah+'!'+reset+bputih+']'+reset+merah+' Canceled by USER!'+reset);sys.exit()
 
-banner = borange+"""
->==========================================================<
- | """+reset+putih+"""           Riddles - Archive File Cracker!!      """+borange+"""      |
-<+========================================================+>
- |"""+reset+hijau+""" Creator:   """+putih+""" Sreetx  """+borange+"""                                   |
- |"""+reset+hijau+""" Language:  """+putih+""" Python3  """+borange+"""                                  |
- |"""+reset+hijau+""" Version:   """+putih+""" 0.0.5  """+borange+"""                                    |
- |"""+reset+hijau+""" Github:   """+putih+"""  https://github.com/Sreetx   """+borange+"""               |
- |"""+reset+hijau+""" YouTube: """+putih+"""   https://www.youtube.com/@linggachannel4781 """+borange+"""|
- >========================================================<"""+reset+bputih+"""
-
- [*] File wordlist bawaan ada di "word/wordlist.txt di script ini!"""+reset
- 
-            
+c = ""
 def zips():
     try:
         def hasil(p):
@@ -88,6 +80,7 @@ def zips():
             filed = """#LOG FILE
 #PASSWORD FOUND
 
+# MODE: ZIP Brute Force
 # Time: """+timestamp+"""
 # Zip File: """+f.split("/")[-1]+"""
 # Directory: """+f+"""
@@ -115,30 +108,65 @@ def zips():
                 fd.write(bite)
             sys.exit()
             os.system("cls||clear")
-        
+    #EXECALGORITMA
+        def jipa():
+            print(kelabu+" ["+banorange+"#"+reset+kelabu+"] Masuk ke brute force mode algoritma"+reset)
+            print(putih+" ["+banorange+"#"+reset+putih+"] Lanjutkan dulu aktifitas anda karena ini mungkin akan sangat lama"+reset)
+            with zipfile.ZipFile(f) as z:
+                for lenght in range(1, int(algoritma.total) + 1):
+                    while True:
+                        p = ''.join(random.choices(algoritma.abjad, k=int(algoritma.total)))
+                        print(kelabu+"\r ["+banhijau+"INFO"+reset+kelabu+"]"+orange+" Wordlist yang dibuat: "+reset+banhijau+str(p)+reset, end='', flush=True)
+                        try:
+                            z.extractall('hasil', pwd=p.encode('utf-8'))
+                            hasil(p)
+                        except (EOFError, KeyboardInterrupt): print(kelabu+'\n ['+banmerah+'!'+reset+kelabu+'] '+merah+' Dibatalkan!'+reset);sys.exit()
+                        except (RuntimeError, zipfile.BadZipFile, zipfile.LargeZipFile, zlib.error): continue
+    
+    #EXECNORMAL
+        def jip():
+            j = len(list(open(tebak, 'rb')))
+            print(kelabu+' ['+banhijau+'*'+reset+kelabu+'] Jumlah kata yang akan dicek:'+hijau, j)
+            with zipfile.ZipFile(f) as z, open(tebak, 'rb') as p:
+                for p in tqdm(p, total=j, unit='word'):
+                    try:
+                        z.extractall('hasil', pwd=p.strip())
+                        hasil(p)
+                    except (EOFError, KeyboardInterrupt): print(kelabu+' ['+banmerah+'!'+reset+kelabu+'] '+merah+' Dibatalkan!'+reset);sys.exit()
+                    except (RuntimeError, zipfile.BadZipFile, zipfile.LargeZipFile, zlib.error): continue
     #ZIP
         print(banner)
         print(kelabu+"\n ["+banorange+"ZIP"+reset+kelabu+"] Anda masuk dalam mode ekstraksi ZIP"+reset)
         f = input(kelabu+' ['+banhijau+'?'+reset+kelabu+'] Masukkan lokasi file zip anda: '+reset)
-        tebak = input(kelabu+' ['+banhijau+'?'+reset+kelabu+'] Masukkan lokasi file wordlist anda: '+reset)
+        algort = input(putih+" ["+banorange+"?"+reset+putih+"] Gunakan algoritma? [y/n]: "+reset)
+        if algort.lower() == "y" or algort.lower() == "Y":
+            try:
+                from Extras import algoritma
+            except ImportError as e:
+                print(e)
+                print(putih+" ["+banmerah+"!"+reset+putih+"] "+merah+"Modul hiang! Install ulang script ini dari repositori gw"+reset);sys.exit()
+        elif algort.lower() == "n":
+            tebak = input(kelabu+' ['+banhijau+'?'+reset+kelabu+'] Masukkan lokasi file wordlist anda: '+reset)
+            if tebak is c:
+                print(borange+' ['+reset+banmerah+'!'+reset+borange+'] '+merah+' Perhatikan penulisan path wordlist'+reset);sys.exit()
+        else:
+            tebak = input(kelabu+' ['+banhijau+'?'+reset+kelabu+'] Masukkan lokasi file wordlist anda: '+reset)
+            if tebak is c:
+                print(borange+' ['+reset+banmerah+'!'+reset+borange+'] '+merah+' Perhatikan penulisan path wordlist'+reset);sys.exit()
         print(putih+' \n['+banorange+'INFO'+reset+putih+'] '+orange+'Jika proses penebakan berhenti, itu artinya script menemukan kata yang tepat dan script mencoba untuk mengekstrak file tersebut'+reset)
         print(putih+'['+banorange+'INFO'+reset+putih+'] '+orange+'Ekstraksi akan segera dilakukan setelah password ditemukan\n'+reset)
       
 # HASH
         try:
             param = zipfile.ZipFile(f)
-        except (FileNotFoundError, zipfile.BadZipFile): print(borange+'\n ['+reset+banmerah+'!'+reset+borange+'] '+merah+'Terdeteksi bukan file zip atau anda salah memasukkan path. Koreksi penulisan path'+reset);sys.exit()
-        if tebak is None:
-            print(borange+' ['+reset+banmerah+'!'+reset+borange+'] '+merah+' Perhatikan penulisan path wordlist'+reset);sys.exit()
-        j = len(list(open(tebak, 'rb')))
-        print(kelabu+' ['+banhijau+'*'+reset+kelabu+'] Jumlah kata yang akan dicek:'+hijau, j)
-        with zipfile.ZipFile(f) as z, open(tebak, 'rb') as p:
-            for p in tqdm(p, total=j, unit='word'):
-                try:
-                    z.extractall('hasil', pwd=p.strip())
-                    hasil(p)
-                except KeyboardInterrupt: print(kelabu+' ['+banmerah+'!'+reset+kelabu+'] '+merah+' Dibatalkan!'+reset);sys.exit()
-                except (RuntimeError, zipfile.BadZipFile, zipfile.LargeZipFile): continue
+        except (FileNotFoundError, zipfile.BadZipFile): print(borange+' ['+reset+banmerah+'!'+reset+borange+'] '+merah+'Terdeteksi bukan file zip atau anda salah memasukkan path. Koreksi penulisan path'+reset);sys.exit()
+        
+        if algort.lower() == "y" or algort.lower() == "Y":
+            jipa()
+        if algort.lower() == "n":
+            jip()
+        else:
+            jip()
         print('Wordlist Habis')
     except (KeyboardInterrupt, EOFError): print(merah+'\n [!] CTRL+C Terdeteksi, keluar dari program....'+reset);sys.exit()
                 
@@ -152,10 +180,11 @@ def rars():
             filed = f"""#LOG FILE
 #PASSWORD FOUND
 
+# MODE: RAR Brute Force
 # Time: {timestamp}
 # Rar File: {r.split("/")[-1]}
 # Directory: {r}
-# Password: {pas.rstrip()}
+# Password: {pas.decode().strip()}
 # Device Model: {hpst}
 
 #################################
@@ -227,19 +256,22 @@ def helpp():
         print(kelabu+" ["+banhijau+"*"+reset+kelabu+"]"+putih+" Terima kasih karena sudah support Kami... :)"+reset)
         webbrowser.open("https://www.youtube.com/@linggachannel4781")
         sys.exit()
-    else: sys.exit()
+    else: pass
     
 def info():
     print(kelabu+"["+banhijau+"!"+reset+kelabu+"] "+putih+" Ketikan python3 "+sys.argv[0]+" --hh Untuk info lebih lanjut(Gk bisa otomatis bjir)\n"+reset)
+    
 menu = optparse.OptionParser(info())
 menu.add_option('--zip', dest="zipp", action='store_true', default=False)
 menu.add_option('--rar', dest="rarr", action='store_true', default=False)
 menu.add_option('--hh', dest="hh", action='store_true', default=False)
+menu.add_option('--7z', dest='szip', action="store_true", default=False)
 
 (option, args) = menu.parse_args()
 rarr = option.rarr
 zipp = option.zipp
 hh = option.hh
+sz = option.szip
 
 if hh:
     os.system("cls||clear")
@@ -250,3 +282,25 @@ if zipp:
 if rarr:
     os.system("cls||clear")
     rars()
+if sz:
+    os.system("cls||clear")
+    print(banner)
+    try:
+        import sevenzippy
+        sevenzip.Sxc()
+    except ImportError:
+        szz = input(putih+" ["+banmerah+"!"+reset+putih+"]"+orange+" Install Riddles - 7Zip Cracker? "+kelabu+"["+banhijau+"y/n"+reset+kelabu+"]: "+reset)
+        if szz.lower() == "y" or szz.lower() == "Y":
+            try:
+                import requests
+                req = requests.get("https://raw.githubusercontent.com/Sreetx/Extras/sevenzip.py")
+                with open("sevenzip.py", "wb") as file:
+                        file.write(req.content)
+                print(borange+" [*] Terinsall! Mulai ulang script sekarang!");sys.exit()
+            except ImportError:
+                print(merah+" [+] Python3-requests akan diinstall terlebih dahulu untuk mengunduh script ini"+reset)
+                os.system("pip install requests")
+                enter = input(kelabu+" ["+banhijau+"?"+reset+kelabu+"] "+orange+"Jika requests terinstall dengan baik, tekan CTRL+C untuk keluar. Jika memikiki masalah dengan pip, tekan enter(for Debian and derivative users")
+                os.system("apt install python3-requests")
+        else:
+            pass
