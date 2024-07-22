4d45b0173b3d4d1f630aa956ef4bd95672356959
Programmer Pemula <98646763+Sreetx@users.noreply.github.com>
2024-07-22 07:12:25 +0700

    Add files via upload

---
+++ src/Extras/algoritma.py
@@ -0,0 +1,27 @@
+
+from Extras.color.warna import kelabu
+from Extras.color.warna import banorange
+from Extras.color.warna import reset
+from Extras.color.warna import banhijau
+from Extras.color.warna import borange
+
+import random, os, sys
+
+print(kelabu+"""["""+banorange+"""SELECT"""+reset+kelabu+"""] Pilih salah satu!"""+borange+"""
+    (1) Gunakan huruf saja
+    (2) Gunakan angka saja
+    (3) Gunakan angka dan huruf saja
+    (4) Gunakan semua termasuk simbol"""+reset)
+number = input(kelabu+" ["+banhijau+"?"+reset+kelabu+"] Pilih salah satu?: "+reset)
+total = input(kelabu+" ["+banhijau+"?"+reset+kelabu+"] Jumlah kata Wordlist yang akan digunakan: "+reset)
+
+if number.lower() == "4":
+    abjad = '''abcdefghijklmnopqrstuvwxyz1234567890@#$_&-+()/*"':;!?~`|•√π÷×¶∆\}{=°^¥€¢£%©®™✓[]"'''
+if number.lower() == "1":
+    abjad = "abcdefghijklmnopqrstuvwxyz"
+if number.lower() == "2":
+    abjad = "1234567890"
+if number.lower() == "3":
+    abjad == "abcdefghijklmnopqrstuvwxyz1234567890"
+
+    
--- src/Extras/banner.py
+++ src/Extras/banner.py
@@ -1,1 +1,33 @@
+try:
+    import sys, os
+    from Extras.color.warna import orange
+    from Extras.color.warna import putih
+    from Extras.color.warna import merah
+    from Extras.color.warna import hijau
+    from Extras.color.warna import biru
+    from Extras.color.warna import borange
+    from Extras.color.warna import bputih
+    from Extras.color.warna import bhijau
+    from Extras.color.warna import bbiru
+    from Extras.color.warna import bmerah
+    from Extras.color.warna import kelabu
+    from Extras.color.warna import borangekelip
+    from Extras.color.warna import banmerah
+    from Extras.color.warna import banhijau
+    from Extras.color.warna import banorange
+    from Extras.color.warna import reset
+except ImportError:
+    print(' [!] Harap install ulang script ini dari repository github kami!');sys.exit()
 
+banner = borange+"""
+>==========================================================<
+ | """+reset+putih+"""           Riddles - Archive File Cracker!!      """+borange+"""      |
+<+========================================================+>
+ |"""+reset+hijau+""" Creator:   """+putih+""" Sreetx  """+borange+"""                                   |
+ |"""+reset+hijau+""" Language:  """+putih+""" Python3  """+borange+"""                                  |
+ |"""+reset+hijau+""" Version:   """+putih+""" 0.0.6  """+borange+"""                                    |
+ |"""+reset+hijau+""" Github:   """+putih+"""  https://github.com/Sreetx   """+borange+"""               |
+ |"""+reset+hijau+""" YouTube: """+putih+"""   https://www.youtube.com/@linggachannel4781 """+borange+"""|
+ >========================================================<"""+reset+bputih+"""
+
+ [*] File wordlist bawaan ada di "word/wordlist.txt di script ini!"""+reset
