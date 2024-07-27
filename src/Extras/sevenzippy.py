a42bf697624f7353e4ff79e18b8885dd03632cce
Programmer Pemula <98646763+Sreetx@users.noreply.github.com>
2024-07-27 09:57:23 +0700

    Add files via upload

--- src/Extras/sevenzippy.py
+++ src/Extras/sevenzippy.py
@@ -1,22 +1,22 @@
 try:
     import sys, os, time
     from banner import banner
-    from color.warna import orange
-    from color.warna import putih
-    from color.warna import merah
-    from color.warna import hijau
-    from color.warna import biru
-    from color.warna import borange
-    from color.warna import bputih
-    from color.warna import bhijau
-    from color.warna import bbiru
-    from color.warna import bmerah
-    from color.warna import kelabu
-    from color.warna import borangekelip
-    from color.warna import banmerah
-    from color.warna import banhijau
-    from color.warna import banorange
-    from color.warna import reset
+    from Extras.color.warna import orange
+    from Extras.color.warna import putih
+    from Extras.color.warna import merah
+    from Extras.color.warna import hijau
+    from Extras.color.warna import biru
+    from Extras.color.warna import borange
+    from Extras.color.warna import bputih
+    from Extras.color.warna import bhijau
+    from Extras.color.warna import bbiru
+    from Extras.color.warna import bmerah
+    from Extras.color.warna import kelabu
+    from Extras.color.warna import borangekelip
+    from Extras.color.warna import banmerah
+    from Extras.color.warna import banhijau
+    from Extras.color.warna import banorange
+    from Extras.color.warna import reset
 except ImportError:
     print(' [!] Harap install ulang script ini dari repository github kami!');sys.exit()
 try:
